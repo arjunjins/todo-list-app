7ba9fe7a12d4777918da02fa5b6a660c226b5f98
Arjun J <arjunjins2001@gmail.com>
2022-11-28 15:47:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//A basic To-Do list app
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+
+  items.push(item);
+  res.redirect("/");
+});
+app.listen(port, () => {
+  console.log("Server started on port 3000");
+});
